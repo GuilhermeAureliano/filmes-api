a1bd138a043aef7ad2dd1e811af54820193b9936
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2023-03-04 23:17:42 -0300

    tests: add testes para os métodos getById e deleteById

--- backend/src/test/java/com/ubots/filmes/services/FilmeServiceTest.java
+++ backend/src/test/java/com/ubots/filmes/services/FilmeServiceTest.java
@@ -1,12 +1,26 @@
 package com.ubots.filmes.services;
 
+import com.ubots.filmes.exceptions.ApiException;
+import com.ubots.filmes.model.Filme;
 import com.ubots.filmes.repository.FilmeRepository;
 import com.ubots.filmes.service.FilmeService;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -18,4 +32,53 @@ public class FilmeServiceTest {
 
     @Autowired
     private FilmeRepository filmeRepository;
+
+    private Filme filme;
+
+    private void criaFilme() {
+        this.filme = new Filme();
+        filme.setName("O Senhor dos Anéis");
+        filme.setDirector("Peter Jackson");
+        filme.setSynopsis("Um jovem querendo proteger um anel com poderes.");
+        filme.setGenre("Aventura");
+        filme.setReleaseYear(LocalDate.parse("2001-12-19"));
+        filme.setAvaliacoes(null);
+        this.filmeRepository.save(filme);
+    }
+
+    @Test
+    public void getById() throws ApiException {
+        this.criaFilme();
+
+        assertEquals("Aventura", filme.getGenre());
+        assertEquals("2001-12-19", String.valueOf(filme.getReleaseYear()));
+
+        try {
+            // Filme não cadastrado
+            this.filmeService.getById(UUID.fromString("a39fed6f-55bd-412f-b84c-31c9623f6f49"));
+        } catch (ApiException e) {
+            assertEquals("Filme não encontrado!", e.getMessage());
+        }
+
+        this.filmeRepository.delete(filme);
+    }
+
+    @Test
+    public void deleteById() throws ApiException {
+        this.criaFilme();
+
+        UUID idTest = filme.getId();
+        this.filmeService.delete(filme.getId());
+        Optional<Filme> filmeOpt = this.filmeRepository.findById(idTest);
+        assertFalse(filmeOpt.isPresent());
+
+        try {
+            // Filme não cadastrado
+            this.filmeService.delete(UUID.fromString("1f8a3082-7dd6-4d4b-9a9f-2e43e66f77d0"));
+        } catch (ApiException e) {
+            assertEquals("Filme não encontrado!", e.getMessage());
+        }
+
+        this.filmeRepository.delete(filme);
+    }
 }
